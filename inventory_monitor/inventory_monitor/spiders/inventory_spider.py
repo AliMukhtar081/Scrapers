2d091e280710d5fcc4d455ff3047e415c15fdda3
AliMukhtar081 <AliMukhtar081@gmail.com>
2023-03-28 13:41:25 +0500

    amazon scrapper

--- inventory_monitor/inventory_monitor/spiders/inventory_spider.py
+++ inventory_monitor/inventory_monitor/spiders/inventory_spider.py
@@ -4,10 +4,10 @@ from inventory_monitor.items import InventoryMonitorItem
 class InventorySpider(scrapy.Spider):
     name = 'inventory_spider'
     allowed_domains = ['example.com']
-    start_urls = ['https://www.example.com/shop/']
+    start_urls = ['https://www.amazon.com/s?k=gaming+headsets&pd_rd_r=a63f202e-7e95-4176-95f0-caf628c482cf&pd_rd_w=7hFdI&pd_rd_wg=7wVvr&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=PDM5GCMDAJT1ZG8RY9Q2&ref=pd_gw_unk']
 
     def parse(self, response):
-        products = response.xpath('//div[@class="product"]')
+        products = response.xpath('//<h2 class="a-size-mini a-spacing-none a-color-base s-line-clamp-2">')
         for product in products:
             item = InventoryMonitorItem()
             item['name'] = product.xpath('.//h3/a/text()').get()
